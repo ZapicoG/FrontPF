4103419bd21d8b14f34566f61532a80dbde739b3
Gonzalo <zapozapico@gmail.com>
2022-10-01 18:18:57 -0300

    getProductsFiltered testing

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -14,7 +14,7 @@ const initialState = {
         model: "",
         search:"",
         minPrice: 0,
-        maxPrice: 100000000000,
+        maxPrice: 2147483647, // Max integer value
         order: "ASC",
         amount: 10,
         page: 0
