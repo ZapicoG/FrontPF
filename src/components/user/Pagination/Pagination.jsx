5e22b94bb9f5f2d9b47633a9ea0fb10cec1a8922
Gonzalo <zapozapico@gmail.com>
2022-10-01 20:31:56 -0300

    Test pagination

--- src/components/user/Pagination/Pagination.jsx
+++ src/components/user/Pagination/Pagination.jsx
@@ -10,7 +10,7 @@ const Pagination = () => {
 
 
 return (<nav aria-label="Page navigation example">
-<ul class="inline-flex -space-x-px">
+{/* <ul class="inline-flex -space-x-px">
   <li>
     <a href="#" onClick={() => {
         if (page >= 0) dispatch(updateFilter({page: (page - 1)}));
@@ -21,7 +21,7 @@ return (<nav aria-label="Page navigation example">
   <li>
     <a href="#" onClick={() => dispatch(updateFilter({page: (page + 1)}))} class="py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
   </li>
-</ul>
+</ul> */}
 </nav>)
     
 }
