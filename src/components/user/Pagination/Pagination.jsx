dd819167a159cab8c8d9379e8e83bee009dd1d7a
Gonzalo <zapozapico@gmail.com>
2022-10-01 20:56:03 -0300

    Test pagination

--- src/components/user/Pagination/Pagination.jsx
+++ src/components/user/Pagination/Pagination.jsx
@@ -20,7 +20,7 @@ return (<nav aria-label="Page navigation example">
         }} class="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
   </li>
         {pages.map((index) => {
-            console.log(index)
+            console.log(index);
             <li>
             <a href="#" 
             // aria-current={ (page == index && page < 3) || page >= 3 && index == 3 ? "page" : false }
