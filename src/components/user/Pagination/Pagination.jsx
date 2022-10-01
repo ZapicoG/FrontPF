ee3ef3e850c72e1d2223cf9b25abeb974bb9816c
Gonzalo <zapozapico@gmail.com>
2022-10-01 20:54:15 -0300

    Test pagination

--- src/components/user/Pagination/Pagination.jsx
+++ src/components/user/Pagination/Pagination.jsx
@@ -15,11 +15,12 @@ return (<nav aria-label="Page navigation example">
 <ul class="inline-flex -space-x-px">
   <li>
     <a href="#" onClick={() => {
-        if (page >= 0) dispatch(updateFilter({page: (page - 1)}));
+        if (page > 0) dispatch(updateFilter({page: (page - 1)}));
         return
         }} class="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
   </li>
         {pages.map((index) => {
+            console.log(index)
             <li>
             <a href="#" 
             // aria-current={ (page == index && page < 3) || page >= 3 && index == 3 ? "page" : false }
