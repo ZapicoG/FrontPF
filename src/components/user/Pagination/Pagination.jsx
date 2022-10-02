aa8489dd9b0d991d477499fd1478d15816e9d990
Gonzalo <zapozapico@gmail.com>
2022-10-01 21:29:08 -0300

    Test pagination

--- src/components/user/Pagination/Pagination.jsx
+++ src/components/user/Pagination/Pagination.jsx
@@ -23,10 +23,10 @@ return (<nav aria-label="Page navigation example">
         {pages.map((index) => {
             return (<li>
             <a href="#" 
-            aria-current={ (page == index && page <= 2) || (page > 2 && index == 3) ? "page" : false }
+            aria-current={ (page == index && page <= 2) || (page > 2 && index == 2) ? "page" : false }
             onClick={() => dispatch(updateFilter({page: (page < 2 ? index : page - 2 + index)}))}
             class={
-                (page == index && page <= 2) || (page > 2 && index == 3)
+                (page == index && page <= 2) || (page > 2 && index == 2)
                 ? "py-2 px-3 text-blue-600 bg-blue-50 border border-gray-300 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white" 
                 : "py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             }
