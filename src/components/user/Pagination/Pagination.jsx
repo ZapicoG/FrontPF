4358109d5efdab4ac11dcde37b3b5a4b8888ba8b
Gonzalo <zapozapico@gmail.com>
2022-10-01 21:27:16 -0300

    Test pagination

--- src/components/user/Pagination/Pagination.jsx
+++ src/components/user/Pagination/Pagination.jsx
@@ -8,6 +8,7 @@ import { updateFilter } from "../../../redux/action";
 const Pagination = () => {
     const dispatch = useDispatch();
     const page = useSelector(state => state.filter.page)
+    // const maxPages = 
     const pages = [0,1,2,3,4];
   
 
@@ -22,10 +23,10 @@ return (<nav aria-label="Page navigation example">
         {pages.map((index) => {
             return (<li>
             <a href="#" 
-            aria-current={ (page == index && page < 2) || (page >= 2 && index == 3) ? "page" : false }
+            aria-current={ (page == index && page <= 2) || (page > 2 && index == 3) ? "page" : false }
             onClick={() => dispatch(updateFilter({page: (page < 2 ? index : page - 2 + index)}))}
             class={
-                (page == index && page < 2) || (page >= 2 && index == 3)
+                (page == index && page <= 2) || (page > 2 && index == 3)
                 ? "py-2 px-3 text-blue-600 bg-blue-50 border border-gray-300 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white" 
                 : "py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             }
