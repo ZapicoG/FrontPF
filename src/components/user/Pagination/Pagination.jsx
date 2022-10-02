79244a2cfb2c32acd791b4c97382b83f78b35a53
Gonzalo <zapozapico@gmail.com>
2022-10-01 21:01:12 -0300

    Test pagination

--- src/components/user/Pagination/Pagination.jsx
+++ src/components/user/Pagination/Pagination.jsx
@@ -20,14 +20,11 @@ return (<nav aria-label="Page navigation example">
         }} class="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
   </li>
         {pages.map((index) => {
-            console.log(index);
             return (<li>
             <a href="#" 
-            // aria-current={ (page == index && page < 3) || page >= 3 && index == 3 ? "page" : false }
-                class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"> { 
-                // page < 3 ? page + index : page - 2 + index 
-                index
-                } </a>
+            aria-current={ (page == index && page < 3) || page >= 3 && index == 3 ? "page" : false }
+            onClick={() => dispatch(updateFilter({page: (page < 3 ? page + index : page - 2 + index)}))}
+                class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"> {page < 3 ? page + index : page - 2 + index} </a>
           </li>)
         })}
   <li>
